7eb7867d656ae4f2139635f7324319e7058c65c1
himanshu khare <khareh57@gmail.com>
2018-12-25 22:13:00 +0530

    setup webpack and test bundle.js file

---
+++ client/components/frame.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Frame extends React.Component {
+    render() {
+        return(
+            <div className="frame">
+                <div className="calculator-title">
+                    R_N_calc
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Frame;
